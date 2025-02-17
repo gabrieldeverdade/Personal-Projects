a90b3e4319d1904efa46ff207257030342dd0b58
Gabriel Azevedo Fernandes <gabri10.fernandes23@gmail.com>
2025-02-17 20:16:08 -0300

    frontend

---
+++ Websites/Replica Spotify/frontend/src/App2.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+// rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá Mundo</h1>
+
+// Nomeação de Componentes
+// PascalCase
+
+// Nomeação de variável, Função...
+// camelCase
+
+// export default posso importar com qualquer nome e sem chaves
+// export "sem default" só posso importar entre chaves e com o nome exportado
+
+// self closing tag <Header></Header> ou </Header>
+
+// Nomeação de classes em CSS
+// Metodologia BEM (BLOCKS, ELEMENTS, MODIFIERS)
+// bloco__elementos--modificador
+// header__link
+// header__link--small
+// item-list__header
+// nomes compostos são separados por -
+
+// tag vazia em React se chama Fragment
