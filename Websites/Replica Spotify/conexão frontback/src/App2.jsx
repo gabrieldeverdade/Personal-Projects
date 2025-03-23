9b6ae8cd0513f573d726f9707393fafb13148f16
Gabriel Azevedo Fernandes <gabri10.fernandes23@gmail.com>
2025-03-23 19:40:34 -0300

    conexão frontback

---
+++ Websites/Replica Spotify/conexão frontback/src/App2.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+// rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá Mundo</h1>
+
+// Nomeação de Componentes
+// PascalCase
+
+// Nomeação de variável, Função...
+// camelCase
+
+// Nomeação de classes
+// kebab-case
+
+// export default posso importar com qualquer nome e sem chaves
+// export "sem default" só posso importar entre chaves e com o nome exportado
+
+// self closing tag <Header></Header> ou </Header>
+
+// Nomeação de classes em CSS
+// Metodologia BEM (BLOCKS, ELEMENTS, MODIFIERS)
+// bloco__elementos--modificador
+// header__link
+// header__link--small
+// item-list__header
+// nomes compostos são separados por -
+
+// tag vazia em React se chama Fragment
+
+//Componentes recebem "props"
+
+// Antes de fazer o MAP do ItemList
+// {items === 5 ? (
+//   <>
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//   </>
+// ) : (
+//   <>
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//     <SingleItem />
+//   </>
+// )}
+
+// {Array(items)
+//   .fill()
+//   .map((currentValue, index) => (
+//     <SingleItem key={`${title}-${index}`} />
+//   ))}
+
+// Spread Operator
+// ...
+
+// Quando componentes se re-renderizam?
+// Uma das ocasiões é quando uma variável de estado usada por esse componente é atualizada
+
+// Hook - useState
